b2bdb4fa6494940112c0e3a722bcde91ac62a35c
Nathan Vande perre <nathanvandeperre@MBP-de-Nathan.home>
2020-06-24 11:50:28 +0200

    feat(Add_item_to_list) : fix merge

--- lib/pages/widget_popup.dart
+++ lib/pages/widget_popup.dart
@@ -1,6 +1,5 @@
 import 'package:MobileOne/localization/localization.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:MobileOne/pages/widget_item.dart';
 import 'package:flutter/material.dart';
 import 'package:MobileOne/pages/openedListPage.dart';
 
