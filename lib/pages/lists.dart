7f2917baf232635fcfe6417b3a7319eba29f893e
zohra051 <37175388+zohra051@users.noreply.github.com>
2020-07-10 17:04:34 +0200

    (fix/guest_see_shared) : you can see lists shared with you on lists page (#54)

--- lib/pages/lists.dart
+++ lib/pages/lists.dart
@@ -1,14 +1,13 @@
 import 'package:MobileOne/localization/localization.dart';
 import 'package:MobileOne/providers/itemsList_provider.dart';
 import 'package:MobileOne/providers/wishlistsList_provider.dart';
+import 'package:MobileOne/services/user_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:MobileOne/widgets/widget_list.dart';
-
-const Color ORANGE = Colors.deepOrange;
 
 class Lists extends StatefulWidget {
   State<StatefulWidget> createState() {
@@ -17,72 +16,106 @@ class Lists extends StatefulWidget {
 }
 
 class ListsState extends State<Lists> {
+  var _userService = GetIt.I.get<UserService>();
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: GetIt.I.get<WishlistsListProvider>(),
       child: Consumer<WishlistsListProvider>(
         builder: (context, wishlistsListProvider, child) {
-          return FutureBuilder<DocumentSnapshot>(
-            future: wishlistsListProvider.ownerLists,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting)
-                return Scaffold(
-                  body: Center(
-                    child: Container(
-                      width: 40,
-                      height: 40,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 1,
-                      ),
-                    ),
+          return Scaffold(
+            body: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 200.0, right: 300),
+                  child: Text(
+                    getString(context, 'my_lists'),
                   ),
-                );
-              else {
-                return content(snapshot.data);
-              }
-            },
+                ),
+                buildFuturBuilderList(wishlistsListProvider),
+                Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: Text(
+                    getString(context, 'shared_with_me'),
+                  ),
+                ),
+                buildFuturBuilderGuest(wishlistsListProvider),
+              ],
+            ),
           );
         },
       ),
     );
   }
 
-  Widget content(DocumentSnapshot snapshot) {
+  buildFuturBuilderList(WishlistsListProvider provider) {
+    return FutureBuilder<DocumentSnapshot>(
+      future: provider.ownerLists,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return CircularProgressIndicator(
+            strokeWidth: 1,
+          );
+        else {
+          return contentList(snapshot.data);
+        }
+      },
+    );
+  }
+
+  Widget contentList(DocumentSnapshot snapshot) {
     final owner = snapshot?.data ?? {};
     final lists = owner["lists"] ?? [];
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 200.0, right: 300),
-            child: Text(
-              getString(context, 'my_lists'),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: lists.length,
-              itemBuilder: (BuildContext ctxt, int index) {
-                return GestureDetector(
-                    onTap: () {
-                      openOpenedListPage(context, lists[index]);
-                    },
-                    child: WidgetLists(
-                        lists[index], getString(context, "shared_count")));
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: lists.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          return GestureDetector(
+              onTap: () {
+                openOpenedListPage(context, lists[index]);
               },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 50.0),
-            child: Text(
-              getString(context, 'shared_with_me'),
-            ),
-          ),
-        ],
+              child: WidgetLists(
+                  lists[index], getString(context, "shared_count")));
+        },
+      ),
+    );
+  }
+
+  buildFuturBuilderGuest(WishlistsListProvider provider) {
+    return FutureBuilder<DocumentSnapshot>(
+      future: provider.guestLists(_userService.user.email),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return CircularProgressIndicator(
+            strokeWidth: 1,
+          );
+        else {
+          return contentGuest(snapshot.data);
+        }
+      },
+    );
+  }
+
+  Widget contentGuest(DocumentSnapshot snapshot) {
+    final guest = snapshot?.data ?? {};
+    final guestList = guest["lists"] ?? [];
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: guestList.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          return GestureDetector(
+              onTap: () {
+                openOpenedListPage(context, guestList[index]);
+              },
+              child: WidgetLists(
+                  guestList[index], getString(context, "shared_count")));
+        },
       ),
     );
   }
