61d714669b627568636aac18688e277a2d26b4f3
zohra051 <37175388+zohra051@users.noreply.github.com>
2020-07-16 14:00:41 +0200

    (fix/contact_loader) : add a loader before getting all contacts (#56)

--- lib/pages/share_one.dart
+++ lib/pages/share_one.dart
@@ -27,13 +27,13 @@ class ShareStateOneState extends State<ShareOne> {
   List<Contact> contactsWithoutEmails = [];
   List<Contact> contactsWithEmails = [];
 
+  bool loader = false;
   bool isVisible = false;
 
   void initState() {
     super.initState();
     newUuid = uuid.v4();
     getContacts();
-
     contactsFilter = [];
     _myController.addListener(() {
       filterContact();
@@ -52,6 +52,7 @@ class ShareStateOneState extends State<ShareOne> {
 
     setState(() {
       contacts = contactsWithEmails.toList();
+      loader = true;
     });
   }
 
@@ -72,181 +73,25 @@ class ShareStateOneState extends State<ShareOne> {
 
   Widget build(BuildContext context) {
     final previousList = ModalRoute.of(context).settings.arguments;
-    final uuid = ModalRoute.of(context).settings.arguments;
     bool isSearching = _myController.text.isNotEmpty;
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 30),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 50,
-              child: Stack(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        openSharePage(uuid);
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 7),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        getString(context, "with_who"),
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 100, right: 100),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                CircleAvatar(
-                  child: Text(
-                    getString(context, "step_one"),
-                    style: TextStyle(
-                      color: WHITE,
-                      fontSize: 10,
-                    ),
-                  ),
-                  radius: 12,
-                  backgroundColor: BLUE,
-                ),
-                Expanded(
-                    child: Divider(
-                  color: Colors.black,
-                  height: 80,
-                  thickness: 1,
-                  indent: 20,
-                  endIndent: 20,
-                )),
-                CircleAvatar(
-                  child: Text(
-                    getString(context, "step_two"),
-                    style: TextStyle(
-                      color: WHITE,
-                      fontSize: 10,
-                    ),
-                  ),
-                  radius: 12,
-                  backgroundColor: GREY,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width - 50,
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: getString(context, "share_email"),
-                labelText: getString(context, 'search'),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: BLUE),
-                ),
-                suffixIcon: Icon(Icons.search),
-              ),
-              controller: _myController,
-              onChanged: (string) {
-                setState(
-                  () {
-                    contactsFilter = contacts
-                        .where((conta) => (conta.displayName
-                                .toLowerCase()
-                                .contains(string.toLowerCase()) ||
-                            conta.emails
-                                .elementAt(0)
-                                .value
-                                .toLowerCase()
-                                .contains(string.toLowerCase())))
-                        .toList();
-                    if (contactsFilter.isEmpty) {
-                      setState(() {
-                        isVisible = true;
-                      });
-                    } else {
-                      setState(
-                        () {
-                          isVisible = false;
-                        },
-                      );
-                    }
-                  },
-                );
-              },
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              searchTermEmail = _myController.text;
-              if (RegExp(emailRegexp).hasMatch(searchTermEmail)) {
-                openShareTwoPage();
-                return null;
-              }
-            },
-            child: Visibility(
-              visible: isVisible,
-              child: Container(
-                margin: const EdgeInsets.all(10.0),
-                color: Colors.amber[600],
-                width: MediaQuery.of(context).size.width - 50,
-                height: 20.0,
-                child: Text("${_myController.text}"),
-              ),
-            ),
-          ),
+          header(context),
+          headerSteps(context),
+          searchContact(context),
+          searchNewContact(context),
           Padding(
             padding: EdgeInsets.only(top: 5),
             child: Container(
               width: MediaQuery.of(context).size.width - 10,
-              height: MediaQuery.of(context).size.height - 269,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                itemCount: isSearching == true
-                    ? contactsFilter.length
-                    : contacts.length,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  contact = isSearching == true
-                      ? contactsFilter[index]
-                      : contacts[index];
-                  return GestureDetector(
-                    onTap: () {
-                      if (isSearching == true) {
-                        contactSelected = contactsFilter[index];
-                      } else {
-                        contactSelected = contacts[index];
-                      }
-                      if (previousList == null || uuid == null) {
-                        openShareTwoPage();
-                      } else {
-                        shareProvider.addSharedToDataBase(
-                            contactSelected.emails.elementAt(0).value,
-                            previousList);
-                        shareProvider.addGuestToDataBase(
-                            contactSelected.emails.elementAt(0).value,
-                            previousList);
-                        openSharePage(uuid);
-                      }
-                    },
-                    child:
-                        WidgetShareContact(contact.displayName, contact.avatar),
-                  );
-                },
-              ),
+              height: MediaQuery.of(context).size.height - 300,
+              child: loader == false
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : buildListView(isSearching, previousList),
             ),
           )
         ],
@@ -254,11 +99,182 @@ class ShareStateOneState extends State<ShareOne> {
     );
   }
 
-  void openSharePage(Object label) {
+  Padding header(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 30),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  openSharePage();
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 7),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  getString(context, "with_who"),
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding headerSteps(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 100, right: 100),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          CircleAvatar(
+            child: Text(
+              getString(context, "step_one"),
+              style: TextStyle(
+                color: WHITE,
+                fontSize: 10,
+              ),
+            ),
+            radius: 12,
+            backgroundColor: BLUE,
+          ),
+          Expanded(
+              child: Divider(
+            color: Colors.black,
+            height: 80,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+          )),
+          CircleAvatar(
+            child: Text(
+              getString(context, "step_two"),
+              style: TextStyle(
+                color: WHITE,
+                fontSize: 10,
+              ),
+            ),
+            radius: 12,
+            backgroundColor: GREY,
+          ),
+        ],
+      ),
+    );
+  }
+
+  GestureDetector searchNewContact(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        searchTermEmail = _myController.text;
+        if (RegExp(emailRegexp).hasMatch(searchTermEmail)) {
+          openShareTwoPage();
+          return null;
+        }
+      },
+      child: Visibility(
+        visible: isVisible,
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          color: Colors.amber[600],
+          width: MediaQuery.of(context).size.width - 50,
+          height: 20.0,
+          child: Text("${_myController.text}"),
+        ),
+      ),
+    );
+  }
+
+  Container searchContact(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 50,
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: getString(context, "share_email"),
+          labelText: getString(context, 'search'),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: BLUE),
+          ),
+          suffixIcon: Icon(Icons.search),
+        ),
+        controller: _myController,
+        onChanged: (string) {
+          setState(
+            () {
+              contactsFilter = contacts
+                  .where((conta) => (conta.displayName
+                          .toLowerCase()
+                          .contains(string.toLowerCase()) ||
+                      conta.emails
+                          .elementAt(0)
+                          .value
+                          .toLowerCase()
+                          .contains(string.toLowerCase())))
+                  .toList();
+              if (contactsFilter.isEmpty) {
+                setState(() {
+                  isVisible = true;
+                });
+              } else {
+                setState(
+                  () {
+                    isVisible = false;
+                  },
+                );
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  ListView buildListView(bool isSearching, Object previousList) {
+    return ListView.builder(
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      itemCount: isSearching == true ? contactsFilter.length : contacts.length,
+      itemBuilder: (BuildContext ctxt, int index) {
+        contact = isSearching == true ? contactsFilter[index] : contacts[index];
+        return GestureDetector(
+          onTap: () {
+            if (isSearching == true) {
+              contactSelected = contactsFilter[index];
+            } else {
+              contactSelected = contacts[index];
+            }
+            if (previousList == null) {
+              openShareTwoPage();
+            } else {
+              shareProvider.addSharedToDataBase(
+                  contactSelected.emails.elementAt(0).value, previousList);
+              shareProvider.addGuestToDataBase(
+                  contactSelected.emails.elementAt(0).value, previousList);
+
+              openSharePage();
+            }
+          },
+          child: WidgetShareContact(contact.displayName, contact.avatar,
+              contact.emails.elementAt(0).value),
+        );
+      },
+    );
+  }
+
+  void openSharePage() {
     Navigator.of(context).pop('/share');
-    if (label != null) {
-      Navigator.of(context).pushNamed('/mainpage');
-    }
   }
 
   void openShareTwoPage() {
--- lib/widgets/widget_share_contact.dart
+++ lib/widgets/widget_share_contact.dart
@@ -1,38 +1,63 @@
 import 'dart:typed_data';
+import 'dart:math';
 import 'package:flutter/material.dart';
-
-const Color GREEN = Colors.green;
 
 class WidgetShareContact extends StatefulWidget {
   final Uint8List avatar;
   final String name;
-  WidgetShareContact(this.name, this.avatar);
+  final String email;
+  WidgetShareContact(this.name, this.avatar, this.email);
   State<StatefulWidget> createState() => WidgetShareContactState();
 }
 
 class WidgetShareContactState extends State<WidgetShareContact> {
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(left: 30),
-          child: (widget.avatar != null && widget.avatar.length > 0)
-              ? CircleAvatar(
-                  backgroundImage: MemoryImage(widget.avatar),
-                )
-              : CircleAvatar(
-                  child: Text(widget.name.substring(0, 2)),
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 30),
+                child: (widget.avatar != null && widget.avatar.length > 0)
+                    ? CircleAvatar(
+                        backgroundImage: MemoryImage(widget.avatar),
+                      )
+                    : CircleAvatar(
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                        child: Text(widget.name.substring(0, 2)),
+                      ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 150),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        widget.name,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        widget.email,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-        ),
-        Text(
-          widget.name,
-          style: TextStyle(
-            fontSize: 15,
+              )
+            ],
           ),
-        ),
-        Container(),
-      ],
+          Container(),
+        ],
+      ),
     );
   }
 }
