33afc77b34f54d5ef9b91c33e90b438ca2a90da6
Vande Perre Nathan <nathan.vande-perre@epitech.eu>
2020-07-27 16:10:58 +0200

    fix : User can now scan different types of barcode

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -57,11 +57,7 @@ class CardsState extends State<Cards> {
                   height: MediaQuery.of(context).size.height * 0.2,
                   width: MediaQuery.of(context).size.width * 0.8,
                   child: Transform(
-                    child: BarcodeWidget(
-                      style: TextStyle(fontSize: 20),
-                      barcode: Barcode.ean13(),
-                      data: args.cards["barecode"],
-                    ),
+                    child: getBarcodeWidget(args.cards),
                     alignment: FractionalOffset.center,
                     transform: new Matrix4.identity()
                       ..rotateZ(90 * 3.1415927 / 180),
@@ -101,6 +97,158 @@ class CardsState extends State<Cards> {
         ],
       ),
     );
+  }
+
+  BarcodeWidget getBarcodeWidget(Map<String, dynamic> _card) {
+    switch (_card["format"]) {
+      case "ean13":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.ean13(),
+          data: _card["barecode"],
+        );
+        break;
+      case "code128":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.code128(),
+          data: _card["barecode"],
+        );
+        break;
+      case "code39":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.code39(),
+          data: _card["barecode"],
+        );
+        break;
+      case "code93":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.code93(),
+          data: _card["barecode"],
+        );
+        break;
+      case "dataMatrix":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.dataMatrix(),
+          data: _card["barecode"],
+        );
+        break;
+      case "ean2":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.ean2(),
+          data: _card["barecode"],
+        );
+        break;
+      case "ean5":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.ean5(),
+          data: _card["barecode"],
+        );
+        break;
+      case "ean8":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.ean8(),
+          data: _card["barecode"],
+        );
+        break;
+      case "gs128":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.gs128(),
+          data: _card["barecode"],
+        );
+        break;
+      case "isbn":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.isbn(),
+          data: _card["barecode"],
+        );
+        break;
+      case "itf":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.itf(),
+          data: _card["barecode"],
+        );
+        break;
+      case "itf14":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.itf14(),
+          data: _card["barecode"],
+        );
+        break;
+      case "pdf417":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.pdf417(),
+          data: _card["barecode"],
+        );
+        break;
+      case "qr":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.qrCode(),
+          data: _card["barecode"],
+        );
+        break;
+      case "rm4scc":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.rm4scc(),
+          data: _card["barecode"],
+        );
+        break;
+      case "aztec":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.aztec(),
+          data: _card["barecode"],
+        );
+        break;
+      case "upce":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.upcE(),
+          data: _card["barecode"],
+        );
+        break;
+      case "upca":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.upcA(),
+          data: _card["barecode"],
+        );
+        break;
+      case "codabar":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.codabar(),
+          data: _card["barecode"],
+        );
+        break;
+      case "telepen":
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.telepen(),
+          data: _card["barecode"],
+        );
+        break;
+      default:
+        return BarcodeWidget(
+          style: TextStyle(fontSize: 20),
+          barcode: Barcode.ean13(),
+          data: _card["barecode"],
+        );
+        break;
+    }
   }
 
   goToLoyaltyCardsPage() {
